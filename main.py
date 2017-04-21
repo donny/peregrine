a66e4728d7ed9a7225bab109a48254bcb45855e8
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-04-21 12:59:03 +1000

    Remove email related files

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import webapp2
-import email_helper
 
 
 class Main(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Send an email to ' + email_helper.SENDER)
+        self.response.write('')
 
 app = webapp2.WSGIApplication([
     ('/', Main),
